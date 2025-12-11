cc514a534a7c349d04c5c08036761cdb9969f0e5
Alex <133679349+alexmen656@users.noreply.github.com>
2025-12-11 21:19:11 +0100

    fix: Reenable cors

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
-//import cors from 'cors';
+import cors from 'cors';
 import fetch from 'node-fetch';
 
 const app = express();
 
-//app.use(cors());
+app.use(cors({
+    origin: [
+        'https://wrapped.digmio.com',
+        'http://localhost:5173',
+    ],
+    credentials: true
+}));
+
 app.use(express.json());
 
 app.get('/api/siege/user/:userId', async (req, res) => {
