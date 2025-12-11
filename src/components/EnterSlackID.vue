ae48354fa91d19d35d4fbc9f3f583e48881ee7d6
Alex <133679349+alexmen656@users.noreply.github.com>
2025-12-11 21:16:52 +0100

    Same thing

--- src/components/EnterSlackID.vue
+++ src/components/EnterSlackID.vue
@@ -25,7 +25,7 @@ const validateAndSubmit = async () => {
   isLoading.value = true
 
   try {
-    const response = await fetch(`http://siege-wrapped-backend.vercel.app/api/siege/user/${slackId.value}`, {
+    const response = await fetch(`https://siege-wrapped-backend.vercel.app/api/siege/user/${slackId.value}`, {
       method: 'GET',
       headers: {
         accept: 'application/json'
